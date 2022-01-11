21b8d52cecb30ea16fa258cbee0542b363bea26d
Anupkumr Seth <anupseth11@yahoo.com>
2022-01-11 22:55:59 +0530

    Adding beans to make dynamic content and also demonstrate processing and transformation

--- camel-microservice-a/src/main/java/com/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
+++ camel-microservice-a/src/main/java/com/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
@@ -3,11 +3,22 @@ package com.microservices.camelmicroservicea.routes.a;
 import java.time.LocalDateTime;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyFirstTimerRouter extends RouteBuilder{
+	
+	@Autowired
+	private GetCurrentTimeBean getCurrentTimeBean;
 
+	
+	@Autowired
+	private SimpleLoggingProcesssingComponent simpleLoggingProcesssingComponent;
+
+	
 	// in configure method we create all the routes.
 	@Override
 	public void configure() throws Exception {
@@ -23,11 +34,36 @@ public class MyFirstTimerRouter extends RouteBuilder{
 		
 		// this is timer endpoint and log end point
 		from("timer:first-timer")
-		//.transform().constant("My Constant message")
-		.transform().constant("Local time : "+ LocalDateTime.now())
+		.log("${body}")
+		.transform().constant("My Constant message")
+		.log("${body}")
+		//.transform().constant("Local time : "+ LocalDateTime.now())
+		.bean(getCurrentTimeBean)
+		.log("${body}")
+		.bean(simpleLoggingProcesssingComponent)
+		.log("${body}")
 		.to("log:first-timer");
 		
 		
 	}
 
+}
+
+@Component
+class GetCurrentTimeBean{
+	
+	public String getCurrentTime() {
+		return "Time now is " + LocalDateTime.now();
+	}
+}
+
+
+@Component
+class SimpleLoggingProcesssingComponent{
+	
+	private Logger logger = LoggerFactory.getLogger(SimpleLoggingProcesssingComponent.class);
+	
+	public void process(String message) {
+		logger.info("SimpleLoggingProcesssingComponent - {}",message);
+	}
 }
