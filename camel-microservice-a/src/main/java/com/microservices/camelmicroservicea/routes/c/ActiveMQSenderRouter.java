35aa36eea11f43f8d39869ad4315f09865214668
Anupkumr Seth <anupseth11@yahoo.com>
2022-01-12 22:34:29 +0530

    created sender router

---
+++ camel-microservice-a/src/main/java/com/microservices/camelmicroservicea/routes/c/ActiveMQSenderRouter.java
@@ -0,0 +1,21 @@
+package com.microservices.camelmicroservicea.routes.c;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveMQSenderRouter extends RouteBuilder{
+
+	@Override
+	public void configure() throws Exception {
+		//timer
+		//transform
+		//queue
+		
+		from("timer:active-mq-timer?period=10000")
+		.transform().constant("My Message for active mq")
+		.log("${body}")
+		.to("activemq:my-activemq-queue");
+	}
+
+}
