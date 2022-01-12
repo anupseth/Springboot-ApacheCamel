a748c7e909f11858f8414171cd3d8a9488814b17
Anupkumr Seth <anupseth11@yahoo.com>
2022-01-12 23:01:05 +0530

    unmarshall json file received from activemq

--- camel-microservice-b/src/main/java/com/microservices/camelmicroserviceb/routes/ActiveMQReceiverRouter.java
+++ camel-microservice-b/src/main/java/com/microservices/camelmicroserviceb/routes/ActiveMQReceiverRouter.java
@@ -1,7 +1,10 @@
 package com.microservices.camelmicroserviceb.routes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
+
+import com.microservices.camelmicroserviceb.pojo.CurrencyExchange;
 
 @Component
 public class ActiveMQReceiverRouter extends RouteBuilder{
@@ -12,8 +15,21 @@ public class ActiveMQReceiverRouter extends RouteBuilder{
 		//transform
 		//queue
 		
+//		from("activemq:my-activemq-queue")
+//		.log("received message from sender : ${body}");
+		
+		//JSON
+		// CurrencyExchange
+//		{
+//			  "id": 1000,
+//			  "from": "USD",
+//			  "to": "INR",
+//			  "conversionMultiple": 70
+//			}
 		from("activemq:my-activemq-queue")
-		.log("${body}");
+		.unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
+		.log("received message from sender : ${body}");
+		
 	}
 
 }
