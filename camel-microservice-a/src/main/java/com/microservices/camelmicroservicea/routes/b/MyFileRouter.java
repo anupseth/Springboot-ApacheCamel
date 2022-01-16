8326c741d8bb56dc70dd1d580a5266da96d99218
Anupkumr Seth <anupseth11@yahoo.com>
2022-01-16 18:49:30 +0530

    Added conditional beans and parameters from request to the bean method

--- camel-microservice-a/src/main/java/com/microservices/camelmicroservicea/routes/b/MyFileRouter.java
+++ camel-microservice-a/src/main/java/com/microservices/camelmicroservicea/routes/b/MyFileRouter.java
@@ -1,10 +1,21 @@
 package com.microservices.camelmicroservicea.routes.b;
 
+import java.util.Map;
+
+import org.apache.camel.Body;
+import org.apache.camel.ExchangeProperties;
+import org.apache.camel.Headers;
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyFileRouter extends RouteBuilder{
+	
+	@Autowired
+	private DeciderBean deciderBean;
 
 	@Override
 	public void configure() throws Exception {
@@ -14,12 +25,13 @@ public class MyFileRouter extends RouteBuilder{
 		.choice()
 			.when(simple("${file:ext} ends with 'xml'"))
 				.log("XML File")
-			.when(simple("${body} contains 'USD'"))
+			//.when(simple("${body} contains 'USD'"))
+				.when(method(deciderBean))
 				.log("Not a XML file but containes USD")
 			.otherwise()
 				.log("Not a XML file")
 		.end()
-		.to("direct://log-file-values")
+		//.to("direct://log-file-values")
 		.to("file:files/output");
 		
 		
@@ -33,4 +45,20 @@ public class MyFileRouter extends RouteBuilder{
 		
 	}
 
+}
+
+@Component
+class DeciderBean{
+	
+	Logger logger = LoggerFactory.getLogger(DeciderBean.class);
+	
+	public boolean isThisConditionMet(@Body String DeciderBean,
+			@Headers Map<String,String> headers,
+			@ExchangeProperties Map<String,String> exchangeProeprties) {
+		
+		logger.info(" Body : {}", DeciderBean);
+		logger.info(" Headers : {}", headers);
+		logger.info(" ExchangeProperties : {}", exchangeProeprties);
+		return true;
+	}
 }
